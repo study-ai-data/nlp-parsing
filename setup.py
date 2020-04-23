b344565126aedffb63fddb3b5b04d03bd55ec909
kimkyeongnam <kkyy0126@naver.com>
2020-04-23 12:07:34 +0900

    Add setup file

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import parsing
+
+"""
+해당 패키지는 pypi에 업로드 하지 않을 것임!
+"""
+
+setup(name=parsing.__name__,
+      description=parsing.__description__,
+      version=parsing.__version__,
+      author=['Kyeongnam Kim'],
+      author_email=['kkyy0126@naver.com'],
+      url=parsing.__url__,
+      #download_url=parsing.__download_url__,
+      install_requires=parsing.__install_requires__,
+      license=parsing.__license__,
+      long_description=open('./README.md', 'r', encoding='utf-8').read(),
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      classifiers=[
+            'Programming Language :: Python :: 3',
+      ]
+      )
