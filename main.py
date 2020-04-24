ae5802218303acdc0eedea39752d08b3defcc067
kimkyeongnam <kkyy0126@naver.com>
2020-04-24 11:16:17 +0900

    Modify def name

---
+++ main.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from parsing.excels import excel_single_file, excel_multi_files, excel_civil_complaint
+from parsing.jsons import *
+from parsing.texts import *
+from parsing.xmls import *
+
+
+# if __name__ == "__main__":
+    # excel_single_file()       # single file
+    # excel_multi_files()     # multi files
+    # excel_civil_complaint()  # civil complaint
--- parsing/excels.py
+++ parsing/excels.py
@@ -37,8 +37,26 @@ class Excel:
         return self.data.sort_index(axis, ascending)
 
     # select data
-    def select(self, col_name):
-        return self.data[col_name]
+    def select(self, col_name1, col_name2=None, select="row"):
+        if select == "row":
+            if col_name2 is None:
+                return self.data[col_name1]
+            else:
+                return self.data[col_name1:col_name2]
+        elif select == "col":
+            if col_name2 is None:
+                return self.data.loc[:, [col_name1]]
+            else:
+                return self.data.loc[:, [col_name1, col_name2]]
+
+    # combine data
+    def combine(self, data1, data2, select="row"):
+        data1 = data1.rename(columns=lambda x: x.replace("question", "data"))
+        data2 = data2.rename(columns=lambda x: x.replace("answer", "data"))
+        if select == "row":
+            return pd.concat([data1, data2], axis=1)
+        elif select == "col":
+            return pd.concat([data1, data2], ignore_index=True)
 
     # save files
     def save(self, filename, data, format=None):
@@ -51,23 +69,36 @@ class Excel:
             data.to_csv(newName, index=False, header=None, sep="\n")
 
 
-def multi_files(filenames):
+def excel_multi_files():
+    filenames = ["A 음식점(15,726).xlsx", "B 의류(15,826).xlsx", "C 학원(4,773).xlsx",
+                 "D 소매점(14,949).xlsx", "E 생활서비스(11,087).xlsx", "F 카페(7,859).xlsx",
+                 "G 숙박업(7,113).xlsx", "H 관광여가오락(4,949).xlsx"
+                 ]
     excels = [Excel("data/"+filename) for filename in filenames]
     for excel in excels:
         sentence = excel.select("SENTENCE")
         excel.save("only_speak", sentence, "txt")
 
 
-if __name__ == "__main__":
-    # single file
-    # filename = "data/" + "test_xlsx.xlsx"
-    # excel = Excel(filename)
-    # sentence = excel.select("SENTENCE")
-    # excel.save("only_speak", sentence, "txt")
+def excel_single_file():
+    filename = "data/" + "test_xlsx.xlsx"
+    excel = Excel(filename)
+    sentence = excel.select("SENTENCE")
+    excel.save("only_speak", sentence, "txt")
 
-    # multi files
-    filenames = ["A 음식점(15,726).xlsx", "B 의류(15,826).xlsx", "C 학원(4,773).xlsx",
-                 "D 소매점(14,949).xlsx", "E 생활서비스(11,087).xlsx", "F 카페(7,859).xlsx",
-                 "G 숙박업(7,113).xlsx", "H 관광여가오락(4,949).xlsx"
-                 ]
-    multi_files(filenames)
+
+def excel_civil_complaint():
+    filenames = ["J 민원 여권 최종본(0416).xlsx", "J 민원 교통_최종본(0416).xlsx",
+                 "J 민원 상수도_최종본(0416).xlsx", "J 민원 차량등록_최종본(0429).xlsx"]
+    excels = [Excel("data/"+filename) for filename in filenames]
+    for excel in excels:
+        question = excel.select("question", select="col")
+        answer = excel.select("answer", select="col")
+        result = excel.combine(question, answer, "col")
+        excel.save("only_speak", result, "txt")
+
+
+if __name__ == "__main__":
+    # excel_single_file()       # single file
+    # excel_multi_files()     # multi files
+    excel_civil_complaint()   # civil complaint
