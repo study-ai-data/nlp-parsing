d22a91b814c9467b341aeeda34fdc756559f2a51
kimkyeongnam <kkyy0126@naver.com>
2020-04-24 14:13:23 +0900

    Add usage of json file

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from parsing.excels import excel_single_file, excel_multi_files, excel_civil_complaint
-from parsing.jsons import *
+from parsing.jsons import json_single_file, json_multi_files
 from parsing.texts import *
 from parsing.xmls import *
 
 
-# if __name__ == "__main__":
-    # excel_single_file()       # single file
-    # excel_multi_files()     # multi files
-    # excel_civil_complaint()  # civil complaint
+if __name__ == "__main__":
+    """ Excel file """
+    excel_single_file()         # single file
+    excel_multi_files()         # multi files
+    excel_civil_complaint()     # civil complaint
+
+    """ Json file """
+    json_single_file()          # single file
+    json_multi_files()          # multi files
